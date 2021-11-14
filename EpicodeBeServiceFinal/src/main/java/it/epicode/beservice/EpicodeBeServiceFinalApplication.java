bfc85d55fbba29966bc47008470cd74772e28ece
LoWallo <91950644+LoWallo@users.noreply.github.com>
2021-11-14 11:40:31 +0100

    Create EpicodeBeServiceFinalApplication.java

---
+++ EpicodeBeServiceFinal/src/main/java/it/epicode/beservice/EpicodeBeServiceFinalApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.beservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeBeServiceFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeBeServiceFinalApplication.class, args);
+	}
+
+}
